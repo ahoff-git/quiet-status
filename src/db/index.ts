d34f251766705fba486a9d7f0187ab2c9b217106
ahoff-git <76222790+ahoff-git@users.noreply.github.com>
2025-08-27 18:12:21 -0700

    require DATABASE_URL

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,6 +1,12 @@
 import postgres from 'postgres';
 import { drizzle } from 'drizzle-orm/postgres-js';
 
-const client = postgres(process.env.DATABASE_URL!);
+const connectionString = process.env.DATABASE_URL;
+
+if (!connectionString) {
+  throw new Error('DATABASE_URL is not set');
+}
+
+const client = postgres(connectionString);
 
 export const db = drizzle(client);
